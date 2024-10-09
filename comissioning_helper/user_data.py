7b4434e5a308ba9ffbeaaf493d500a344577676f
Damir Khakimov <damir.hakimov@gmail.com>
2024-10-09 15:18:59 +0400

    user_data.py copied to commision helper

---
+++ comissioning_helper/user_data.py
@@ -0,0 +1,15 @@
+# Constants for clarity
+import os
+import pathlib
+
+basedir = os.path.dirname(__file__)
+asset_dir = os.path.join(basedir, 'asset')
+
+def get_user_data_path():
+    """Returns the user's data directory in an OS-independent way."""
+
+    home_dir = pathlib.Path.home()
+    app_data_dir = home_dir / "AppData" if os.name == "nt" else home_dir / ".config"
+    user_data_dir = app_data_dir / f"ComHelper"
+
+    return user_data_dir
